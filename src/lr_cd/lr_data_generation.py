c11589fb19ca23b21938a6a61f516e7eebdd4690
fohy24 <fohoyin@gmail.com>
2024-01-10 16:46:17 -0800

    docstring for data generation function

---
+++ src/lr_cd/lr_data_generation.py
@@ -0,0 +1,25 @@
+# lr_data_generation.py
+# author: Sam Fo
+# date: 2024-01-10
+
+def generate_data_lr(n, theta, random_seed=123):
+    """Generate a number of data points (X, y) base on the theta coefficients.
+
+    Parameters
+    ----------
+    n : integer
+        The number of data points.
+    theta : float
+        The true theta coefficient for the logistic regression
+    random_seed : integer
+        Random seed to ensure reproducibility.
+
+    Returns
+    -------
+    A dataframe containing the data points (X, y), where X is the randomly generated regressor values, and y is the corresponding binary target.
+
+    Examples
+    --------
+    >>> from lr_cd.lr_data_generation import generate_data_lr
+    >>> generated_data = generate_data_lr(100, 3, 123)
+    """
