ff1a72f3fc3d98b7ba2bae23e323d188602042a6
fohy24 <fohoyin@gmail.com>
2024-01-16 08:42:52 -0800

    feat: added data generation function

--- src/lr_cd/lr_data_generation.py
+++ src/lr_cd/lr_data_generation.py
@@ -2,15 +2,21 @@
 # author: Sam Fo
 # date: 2024-01-10
 
-def generate_data_lr(n, theta, random_seed=123):
+import numpy as np
+
+
+def generate_data_lr(n, n_features, theta, random_seed=123):
     """Generate a number of data points base on the theta coefficients.
 
     Parameters
     ----------
     n : integer
         The number of data points.
+    n_features : ndarray
+        The number of features to generate, excluding the intercept.
     theta : ndarray
-        The true scalar intercept and coefficient weights vector
+        The true scalar intercept and coefficient weights vector.
+        The first element should always be the intercept.
     random_seed : integer
         Random seed to ensure reproducibility.
 
@@ -24,5 +30,16 @@ def generate_data_lr(n, theta, random_seed=123):
     Examples
     --------
     >>> from lr_cd.lr_data_generation import generate_data_lr
-    >>> generated_data = generate_data_lr(100, 3, 123)
+    >>> theta = np.array([4, 3])
+    >>> generate_data_lr(n=10, n_features=1, theta=theta)
     """
+    np.random.seed(random_seed)
+
+    if len(theta) != n_features + 1:
+        raise ValueError('Number of features does not match with theta.')
+
+    X = np.random.normal(size=n * n_features).reshape(n, n_features)
+    true_intercept = theta[0]
+    true_coeff = theta[1:]
+    y = X * true_coeff.reshape(n_features, -1) + true_intercept
+    return X, y
