e65de0db9828ce1d4f2c8476b77c025aa831df36
andyz22 <andyz22@student.ubc.ca>
2024-01-16 14:46:40 -0800

    change tests and add tests

--- tests/test_lr_cd.py
+++ tests/test_lr_cd.py
@@ -9,23 +9,27 @@ import os
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
-
-# Import the train_test_split_class function from the src folder
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from src.lr_cd import coordinate_descent
+from lr_cd.lr_cd import coordinate_descent
 
 
 np.random.seed(666666)
+X = 2 * np.random.rand(100, 5)
+y = 4 + 3 * X[:, 0] + 2 * X[:, 1] + 1.5 * X[:, 2] + 0.5 * X[:, 3] + np.random.randn(100)
+y = y.reshape(-1, 1)
+
+
+
 # Test for correct return type
-def test_result():
-    
-	X = 2 * np.random.rand(100, 5)
-	y = 4 + 3 * X[:, 0] + 2 * X[:, 1] + 1.5 * X[:, 2] + 0.5 * X[:, 3] + np.random.randn(100)
-	y = y.reshape(-1, 1)
+def test_lr_cd():
 
 	lin_reg = LinearRegression()
 	lin_reg.fit(X, y)
 	intercept, coef, _ = coordinate_descent(X, y)
+
+	assert isinstance(X, np.ndarray)
+	assert isinstance(y, np.ndarray)
+	assert isinstance(intercept, float)
+	assert isinstance(coef, np.ndarray)
 
 	expected_result_1 = lin_reg.intercept_
 	calculated_result_1 = intercept
@@ -34,12 +38,6 @@ def test_result():
 
 	assert np.allclose(expected_result_1, calculated_result_1, atol=1e-5)
 	assert np.allclose(expected_result_2, calculated_result_2, atol=1e-5)
-
-
-
-
-
-
 
 
 
